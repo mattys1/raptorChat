eb0b8db9545d913ca78f07323fc1f230bf57915e
dasho0 <150513972+dasho0@users.noreply.github.com>
2025-03-19 14:06:40 +0100

    add changesto scripting

--- deploy_backend.sh
+++ deploy_backend.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
-
 args = "$@"
 
 docker compose down &&
-docker-compose up sqlc --build &&
-docker-compose up backend --build --force-recreate $args
+docker compose up sqlc --build &&
+docker compose up backend --build --force-recreate $args
