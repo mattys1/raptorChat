ae4280ffdc7be1753172093a696a3a5599893856
mattys1 <mattys0082@gmail.com>
2025-03-29 20:58:51 +0100

    add settings hook

--- frontend/src/ui/views/SettingsMain.tsx
+++ frontend/src/ui/views/SettingsMain.tsx
@@ -1,49 +1,29 @@
 import { NavigateFunction } from "react-router-dom";
 
-const SettingsMain = ({navigate}: {navigate: NavigateFunction} ) => {
-  const handleChangeUsername = async () => {
-    // try {
-    //   const response = await fetch("http://localhost:8080/change-username", {
-    //     method: "POST",
-    //     headers: { "Content-Type": "application/json" },
-    //     body: JSON.stringify({ newUsername: "dummy" }),
-    //   });
-    // } catch (error) {
-    //   console.error("Error changing username", error);
-    // }
-  };
+import { useSettingsHook } from "../hooks/useSettingsHook";
 
-  const handleChangePassword = async () => {
-    // try {
-    //   const response = await fetch("http://localhost:8080/change-password", {
-    //     method: "POST",
-    //     headers: { "Content-Type": "application/json" },
-    //     body: JSON.stringify({ newPassword: "dummy" }),
-    //   });
-    // } catch (error) {
-    //   console.error("Error changing password", error);
-    // }
-  };
+const SettingsView = (navigate: NavigateFunction) => {
+	const { handleChangeUsername, handleChangePassword } = useSettingsHook(navigate);
 
-  return (
-    <div>
-      <h1>Settings</h1>
-      <ul style={{ listStyle: "none", padding: 0 }}>
-        <li>
-          <button onClick={handleChangeUsername}>Change Username</button>
-        </li>
-        <li>
-          <button onClick={handleChangePassword}>Change Password</button>
-        </li>
-        <li>
-          <button onClick={() => navigate("/login")}>Log Out</button>
-        </li>
-        <li>
-          <button onClick={() => navigate("/main")}>Return to Start Screen</button>
-        </li>
-      </ul>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Settings</h1>
+			<ul style={{ listStyle: "none", padding: 0 }}>
+				<li>
+					<button onClick={handleChangeUsername}>Change Username</button>
+				</li>
+				<li>
+					<button onClick={handleChangePassword}>Change Password</button>
+				</li>
+				<li>
+					<button onClick={() => navigate("/login")}>Log Out</button>
+				</li>
+				<li>
+					<button onClick={() => navigate("/main")}>Return to Start Screen</button>
+				</li>
+			</ul>
+		</div>
+	);
 };
 
-export default SettingsMain;
+export default SettingsView;
