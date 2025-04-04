e912a67b093d28995aa2c50bb1a712e1669dde91
mattys1 <mattys0082@gmail.com>
2025-04-04 13:00:03 +0200

    more seeds

---
+++ backend/db/seeds/add_test_messages.sql
@@ -0,0 +1,3 @@
+INSERT INTO messages (room_id, sender_id, contents) VALUES (1, 1, "impra w berlinie 1945");
+INSERT INTO messages (room_id, sender_id, contents) VALUES (1, 2, "edward rydz smigly uciekl do argentyny");
+INSERT INTO messages (room_id, sender_id, contents) VALUES (2, 1, "jp2gmd");
