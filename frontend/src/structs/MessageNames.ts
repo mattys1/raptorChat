d3a3f7550be60948a1867af4c596234b557a736e
mattys1 <mattys0082@gmail.com>
2025-04-21 11:17:49 +0200

    convert message enums to const objects

--- frontend/src/structs/MessageNames.ts
+++ frontend/src/structs/MessageNames.ts
@@ -1,14 +1,18 @@
-export enum MessageTypes {
-	SUBSCRIBE = "subscribe",
-	UNSUBSCRIBE = "unsubscribe",
-	CREATE = "create",
-	DELETE = "delete",
-	UPDATE = "update",
-}
+export const MessageTypes = {
+	SUBSCRIBE: "subscribe",
+	UNSUBSCRIBE: "unsubscribe",
+	CREATE: "create",
+	DELETE: "delete",
+	UPDATE: "update",
+} as const;
 
-export enum MessageEvents {
-	MESSAGES = "messages",
-	USERS = "users",
-	ROOMS = "rooms",
-	CHAT_MESSAGES = "chat_messages",
-}
+export type MessageTypes = typeof MessageTypes[keyof typeof MessageTypes];
+
+export const MessageEvents = {
+	MESSAGES: "messages",
+	USERS: "users",
+	ROOMS: "rooms",
+	CHAT_MESSAGES: "chat_messages",
+} as const;
+
+export type MessageEvents = typeof MessageEvents[keyof typeof MessageEvents];
