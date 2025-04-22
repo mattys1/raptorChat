4de83971c56b8fb78b82a30ab18810629d40db03
mattys1 <mattys0082@gmail.com>
2025-04-22 11:49:07 +0200

    temp disable swagger for faster builds

--- deploy_backend.sh
+++ deploy_backend.sh
@@ -2,4 +2,4 @@ args="$@"
 
 docker compose down &&
 docker compose up sqlc --build &&
-DOCKER_BUILDKIT=1 BUILDKIT_INLINE_CACHE=1 docker compose up backend swagger --build --force-recreate $args
+DOCKER_BUILDKIT=1 BUILDKIT_INLINE_CACHE=1 docker compose up backend --build --force-recreate $args
