24a712e9c206759388bcdab36f674505d39bd0e5
dasho0 <150513972+dasho0@users.noreply.github.com>
2025-05-13 02:05:09 +0200

    things

--- backend/src/pkg/db/db.go
+++ backend/src/pkg/db/db.go
@@ -24,10 +24,6 @@ type Queries struct {
 	db DBTX
 }
 
-func (q *Queries) CreateUser(context context.Context, param any) any {
-	panic("unimplemented")
-}
-
 func (q *Queries) WithTx(tx *sql.Tx) *Queries {
 	return &Queries{
 		db: tx,
