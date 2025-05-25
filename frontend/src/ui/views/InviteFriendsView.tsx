3f9d32bf9d570a14b88f2c0e189bb8462c2ed0ed
Dawid Szołdra <150513972+dasho0@users.noreply.github.com>
2025-05-25 15:38:07 +0200

    Changed styles in invite friends

--- frontend/src/ui/views/InviteFriendsView.tsx
+++ frontend/src/ui/views/InviteFriendsView.tsx
@@ -1,19 +1,29 @@
-import { useParams } from "react-router-dom"
-import { useInviteToChatroomHook } from "../hooks/views/useInviteToChatroomHook"
-import IssueInvite from "../components/IssueInvite"
-import { useInviteFriendsHook } from "../hooks/views/useInviteFriendsHook"
+import React from "react";
+import { useParams } from "react-router-dom";
+import IssueInvite from "../components/IssueInvite";
+import { useInviteFriendsHook } from "../hooks/views/useInviteFriendsHook";
 
-const InviteFriendsView = () => {
-	const roomId = Number(useParams().chatId)
-	console.log("InviteToChatroomView roomId", roomId)
-	const props = useInviteFriendsHook(roomId)
+const InviteFriendsView: React.FC = () => {
+  const roomId = Number(useParams<{ chatId: string }>().chatId);
+  const props = useInviteFriendsHook(roomId);
 
-	return <>
-		Invite Friend<br/>
-		{props.nonFriends.map(user => {
-			return <IssueInvite user={user} type="direct" roomId={roomId} />
-		})}
-	</>
-}
+  return (
+    <div className="flex-1 bg-[#394A59] min-h-screen flex justify-start p-8">
+      <div className="w-full max-w-lg bg-[#1E2B3A] text-white rounded-lg shadow p-6 space-y-4">
+        <h1 className="text-2xl font-bold">Invite Friend</h1>
+        <div className="space-y-2">
+          {props.nonFriends.map((user) => (
+            <IssueInvite
+              key={user.id}
+              user={user}
+              type="direct"
+              roomId={roomId}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default InviteFriendsView
+export default InviteFriendsView;
