94782ce9a8096faf1a7a27b1fa555f6c6b34eedb
mattys1 <mattys0082@gmail.com>
2025-06-09 16:45:19 +0200

    fix random

--- backend/src/pkg/api/handlers/rooms.go
+++ backend/src/pkg/api/handlers/rooms.go
@@ -28,7 +28,7 @@ func GetMessagesOfRoomHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, fmt.Sprintf("Internal Server Error, couldn't retrieve messages of id: %d", roomid), http.StatusInternalServerError)
 	}
 
-	err = SendResource(slices.DeleteFunc(messages, func(message db.Message) bool { return message.IsDeleted.Bool == true }), w)
+	err = SendResource(slices.DeleteFunc(messages, func(message db.Message) bool { return *message.IsDeleted == true }), w)
 	if err != nil {
 		slog.Error("Error sending messages of room", "roomid", roomid, "error", err)
 	}
